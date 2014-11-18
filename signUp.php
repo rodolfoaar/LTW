8aaee6ed836cd07d622d7d8347222704036dcbe2
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-11-18 09:50:58 +0000

    Minor changes related with folder organization

--- signUp.php
+++ signUp.php
@@ -3,7 +3,7 @@
 	//establish connection to the database
 	try
 	{
-		$dbh = new PDO('sqlite:phplogin.db');
+		$dbh = new PDO('sqlite:db/phplogin.db');
 		$dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
