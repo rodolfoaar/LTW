9e25b2166ebbd4bffb59ba9de4918bdc27baa008
4Lfeup <ajblxperia@gmail.com>
2014-11-20 11:54:39 +0000

    index update

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<?
-  include_once('database/connection.php'); 
-  include_once('database/post.php');
-
-  try {
-    $posts = getAllPosts($dbh);
-  } catch (PDOException $e) {
-    die($e->getMessage());
-  }
-
-  include ('templates/header.php');
-  include ('templates/posts.php');
-  include ('templates/footer.php'); 
-?>
-
 <!DOCTYPE html>
 <html>
 	<head>
