977db9866df1e2f014f3ef0a417c1bbd0c9db823
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-11-22 21:44:52 +0000

    Minor changed

--- templates/header.php
+++ templates/header.php
@@ -21,7 +21,7 @@
 	<body>
 
 	<div id="header">
-		<h1>Wellcome to the LTW web application where users can create, share, and manage polls!</h1>
+		<h1>Welcome to the LTW web application where users can create, share, and manage polls!</h1>
 		<h2>=== FEUP 2014 ===</h2>
 		<img id="feupLogo" src="images/Logo-FEUP.gif" alt="FEUP logo" width="232" height="200">
 	</div>
