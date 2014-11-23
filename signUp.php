adc4c9c6edba5c148c918bf7bdb07e02daccce8d
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-11-23 23:12:59 +0000

    sign Up updated with functions from class User

--- signUp.php
+++ signUp.php
@@ -1,64 +1,22 @@
 <?php
 
-	require_once 'init.php';
+session_set_cookie_params(0);
+session_start();
 
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	$confirmPassword = $_POST['confirmPassword'];
-	$age = $_POST['age'];
-	$gender = $_POST['gender'];
-	$email = $_POST['email'];
-	
-	//get username and password
-	if($username && $password && $confirmPassword)
-	{
-		if($username == " ")
-			die("Username must have at least one letter");
 
-		if($password != $confirmPassword)
-			die("Incorrect password!");
+require_once 'database/user.php';
 
-		//verify if the username is already taken
-		try
-		{
-			$stmt_01 = $dbh->prepare("SELECT * FROM users WHERE username ='$username'");
-			$stmt_01->execute();
-			$result = $stmt_01->fetchAll();
+$user = new User();
 
-			if(count($result) != 0)
-			{
+$userInfo = array(
+    'username' => $_POST['username'],
+    'password' => $_POST['password'],
+    'confirmPassword' => $_POST['confirmPassword'],
+    'age' => $_POST['age'],
+    'gender' => $_POST['gender'],
+    'email' => $_POST['email']);
 
-				//print_r($result);
+$user->createUser($userInfo);
+header('Location: view_user.php');
 
-				die("The username is already in use!");
-			}
-				
-			
-		}
-		catch (PDOException $e)
-		{
-			die($e->getMessage());
-		}
-
-		//create user
-		try
-		{
-			$stmt_02 = $dbh->prepare('INSERT INTO users (username, password, age, gender, email) 
-				VALUES (:username, :password, :age, :gender, :email)');
-			$stmt_02->bindParam(':username', $username);
-			$stmt_02->bindParam(':password', md5($password));
-			$stmt_02->bindParam(':age', $age);
-			$stmt_02->bindParam(':gender', $gender);
-			$stmt_02->bindParam(':email', $email);
-			$stmt_02->execute();
-
-			echo "Successful registration";
-		}
-		catch (PDOException $e)
-		{
-			die($e->getMessage());
-		}
-	}
-	else
-		die("Enter username and password");
 ?>
