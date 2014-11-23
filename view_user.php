e99899375806a0941a3e7c118172f396cc4043d3
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-11-23 16:28:57 +0000

    Added view_user.php

---
+++ view_user.php
@@ -0,0 +1,23 @@
+<?php
+
+session_set_cookie_params(0);
+session_start();
+
+require_once 'database/user.php';
+
+$user = new User();
+
+//If user is NOT logged in, redirects the browser to index.php
+if(!$user->isUserLoggedIn())
+{
+    header('Location: index.php');
+    die();
+}
+
+include ('templates/header.php');
+
+?>
+
+    <h1>Registered User Home Page</h1>
+
+<?php include ('templates/footer.php');?>
