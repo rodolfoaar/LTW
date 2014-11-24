b1ebca6135656beeeef101df196fde21e2566e59
4Lfeup <ajblxperia@gmail.com>
2014-11-24 19:37:19 +0000

    create poll with only one question...

--- createPoll.php
+++ createPoll.php
@@ -3,82 +3,49 @@
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
 		<script>
-			var choices = 1;
-			var pollCreated = false;
+			var i = 1;
 
-			function insertQuestion()
+			function addChoiceF()
 			{
-				var text = "<br> question:";
-				var field = "<br> <input type='text' name='question' >";
+				i++;
+				var div = document.createElement('div');
+				var text = 'Insert choice:<br>';
+				var inputText = '<input type="text" name="pollChoice_'+i+'" required="required">';
+				var inputButtonAdd = '<input type="button" id="addChoice()" onClick="addChoiceF()" value="+">';
+				var inputButtonRemove = '<input type="button" onClick="removeChoiceF(this)" value="-">';
+				div.innerHTML = text+inputText+inputButtonAdd+inputButtonRemove;
+				document.getElementById('choice').appendChild(div);
 
-				if(!pollCreated)
-				{
-					
-					$("#question").append(text, field);
-					$("#inserQuestion").hide();
-					$("#insertChoice").show();
-				}
-				else
-				{
-					$("#question").append(text, field);
-					$("#inserQuestion").hide();
-					$("#insertChoice").show();
-				}
-			}
-
-			function insertChoise()
-			{
-				var text = "<br> choise:";
-				var field = "<br> <input type='text' name='choice"+choices+"'>";
-				$("#choice").append(text, field);
-				choices++;
-
-				if(choices > 2)
-				{
+				if(i >= 2)
 					$("#submitPoll").show();
-				}
 			}
 
-			function submitQuestion()
+			function removeChoiceF(div)
 			{
-				pollCreated = true;
+				document.getElementById('choice').removeChild(div.parentNode);
+				i--;
 
-				var numberOfOptions = choices - 1;
-				var optionNumber, option;
-
-				console.log($("input:text[name=pollTitle]").val());
-				console.log($("input:text[name=question]").val());
-
-				for(var i=1; i<=numberOfOptions; i++)
-				{
-					optionNumber = "input:text[name=choice"+i+"]";
-					option = $(optionNumber).val();
-					console.log(option);
-				}
-
-				$("#inserQuestion").show();
-				choices = 1;
-				$("#choice").empty();
-				$("#question").empty();
-				$("#insertChoice").hide();
-				$("#submitPoll").hide();
-				$("input:text").prop("disabled",true);
+				if(i < 2)
+					$("#submitPoll").hide();
 			}
 		</script>
 
 	</head>
 	<body>
-		<fieldset>
-			<legend>Poll</legend>
-			Insert poll title:<br>
-			<input type='text' name='pollTitle' required="required"> <br>
-			<section id="question">
-			</section>
-			<section id="choice">
-			</section>
-		</fieldset> 
-		<button id="inserQuestion" onclick="insertQuestion()">Insert question</button>
-		<button id="insertChoice" onclick="insertChoise()" hidden="hidden">Insert choise</button>
-		<button id="submitPoll" onclick="submitQuestion()" hidden="hidden">Submit poll</button> 
+		<form action="insertPoll.php" method="POST">
+			<fieldset>
+				<legend>Poll</legend>
+				Insert title:<br>
+				<input type="text" name="pollTitle" required="required"> <br>
+				Insert question:<br>
+				<input type="text" name="pollQuestion" required="required"> <br>
+				<div id="choice">
+					Insert choice:<br>
+					<input type="text" name="pollChoice_1" required="required">
+					<input type="button" id="addChoice()" onClick="addChoiceF()" value="+">
+				</div>
+			</fieldset> 
+			<input type="submit" id="submitPoll" value="Submit poll" hidden="hidden">
+		</form>
 	</body>
 </html>
