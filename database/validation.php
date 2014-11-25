d1efc0d8bd6bc3c130fecdbef943d9dafa905db0
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-11-25 11:25:19 +0000

    sign in validation

--- database/validation.php
+++ database/validation.php
@@ -1,57 +1,92 @@
 <?php
 
-function validateSignIn($username, $password)
+class Validation
 {
-    session_unset();
+    private $submit_sign_in;
 
-    $submit_sign_in = true;
-    $_SESSION['formSignIn']['username'] = $_POST['username_sign_in'];
-    $_SESSION['formSignIn']['password'] = $_POST['password_sign_in'];
-
-    $data = cleanSpacesSlashes($username);
-
-    if (!isset($data) || $data === '' || strlen($data) > 20) {
-        $_SESSION['errorSignIn']['username'] = "Invalid username.";
-        $submit_sign_in = false;
-    }
-
-    $info_sign_in = array('username' => $data);
-
-    $data = cleanSpacesSlashes($password);
-
-    if(!isset($data) || $data === '')
-    {
-        $_SESSION['errorSignIn']['password'] = "Invalid password.";
-        $submit_sign_in = false;
-    }
-
-    $info_sign_in['password'] = $data;
-
-    if($submit_sign_in)
+    public function validateSignIn($username, $password)
     {
         session_unset();
-        return $info_sign_in;
-    }
 
-    header('Location: index.php');
-    die();
-}
+        $this->submit_sign_in = true;
+        $_SESSION['formSignIn']['username'] = $username;
+        $_SESSION['formSignIn']['password'] = $password;
+
+        $data = $this->validateUsername($username);
+
+        $info_sign_in = array('username' => $data);
+
+        $data = $this->validatePassword($password);
+
+        $info_sign_in['password'] = $data;
+
+        if ($this->submit_sign_in) {
+            session_unset();
+            return $info_sign_in;
+        }
+
+        header('Location: index.php');
+        die();
+    }
 
 //========================================
 
-function cleanSpacesSlashes($formField)
-{
-    $data = trim($formField);
-    $data = stripslashes($data);
-    return $data;
+    public function validateUsername($un)
+    {
+        $data = $this->cleanInput($un);
+
+        if (empty($un)) {
+            $_SESSION['errorSignIn']['username'] = "Username is required.";
+            $this->submit_sign_in = false;
+            return false;
+        } else {
+
+            if (!isset($data) || $data === '' || strlen($data) > 20) {
+                $_SESSION['errorSignIn']['username'] = "Invalid username.";
+                $this->submit_sign_in = false;
+                return false;
+            }
+        }
+
+        return $data;
+    }
+
+//========================================
+
+    public function validatePassword($pass)
+    {
+        $data = $this->cleanInput($pass);
+
+        if (empty($pass)) {
+            $_SESSION['errorSignIn']['password'] = "Password is required.";
+            $this->submit_sign_in = false;
+            return false;
+        } else {
+
+            if (!isset($data) || $data === '') {
+                $_SESSION['errorSignIn']['password'] = "Invalid password.";
+                $this->submit_sign_in = false;
+            }
+        }
+
+        return $data;
+    }
+
+//========================================
+
+    public function cleanInput($formField)
+    {
+        $data = trim($formField);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 }
 
 //========================================
 
 function getFieldVal($fv)
 {
-    if(isset($fv))
-    {
+    if (isset($fv))
         return $fv;
-    }
 }
--- sign_in.php
+++ sign_in.php
@@ -6,7 +6,8 @@ session_start();
 require_once 'database/user.php';
 require_once 'database/validation.php';
 
-$info_sign_in = validateSignIn($_POST['username_sign_in'], $_POST['password_sign_in']);
+$valid = new Validation();
+$info_sign_in = $valid->validateSignIn($_POST['username_sign_in'], $_POST['password_sign_in']);
 
 $user = new User();
 $user->logInUser($info_sign_in['username'], $info_sign_in['password']);
