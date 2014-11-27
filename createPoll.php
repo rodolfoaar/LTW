11677f73b912d4ce8f3ce1ce94955dfbb51d9d63
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-11-27 21:23:01 +0000

    Poll questions and choices numeration added

--- createPoll.php
+++ createPoll.php
@@ -1,17 +1,17 @@
-<form id="insert_poll" action="insertPoll.php" method="POST">
+<form id="insert_poll" action="save_poll.php" method="POST">
 
     <fieldset>
         <legend>Poll</legend>
 
         <p>Insert title: <input id="poll_title" type="text" name="pollTitle" required="required"></p>
 
-        <p>Insert image: <input type="file" name="picture" required="required"></p>
+        <p>Insert image: <input type="file" name="picture" ></p>
 
         <ul id="poll_list">
 
             <li class="question">
                 <p>Insert question:
-                    <input class="poll_question" type="text" name="pollQuestion" required="required">
+                    <input class="poll_question" type="text" name="pollQuestion_1" required="required">
                     <input type="button" id="remove_question" value="Remove question" hidden>
                 </p>
 
@@ -19,11 +19,11 @@
                     <p>Insert choices:</p>
 
                     <li>
-                        <input type="text" name="pollChoice" >
+                        <input type="text" name="pollChoice_2" >
                     </li>
 
                     <li>
-                        <input type="text" name="pollChoice" >
+                        <input type="text" name="pollChoice_3" >
                         <input type="button" id="add_question" onclick="addChoiceF(event)" value="+" >
                     </li>
 
--- js/user.js
+++ js/user.js
@@ -3,34 +3,47 @@ $(setUp);
 
 //Initial setup
 function setUp(){
-    $("#insert_poll").submit(insertPoll)
+    //$("#insert_poll").submit(insertPoll)
     $("#add_another_question").click(addQuestionF);
 }
 
 //============================================================
 
-var choice_block = $('<li><input type="text" name="pollChoice" > <input type="button" id="add_question" onclick="addChoiceF(event)" value="+" > <input type="button" id="remove_choice" onclick="removeChoiceF(event)" value="-" ></li>');
-
+var choice_block = $('<li><input type="text" id="choice" name="pollChoice" > <input type="button" id="add_question" onclick="addChoiceF(event)" value="+" > <input type="button" id="remove_choice" onclick="removeChoiceF(event)" value="-" ></li>');
+var ref_num = 3;
 //============================================================
 
 function addQuestionF(event)
 {
     var question_block = $('<li class="question"> \
                                 <p>Insert question: \
-                                    <input class="poll_question" type="text" name="pollQuestion" required="required"> \
+                                    <input class="poll_question" type="text" id="question" name="pollQuestion" required="required"> \
                                     <input type="button" id="remove_question" onclick="removeQuestionF(event)" value="Remove question" > \
                                 </p> \
                                 <ul> \
                                     <p>Insert choices:</p> \
                                     <li> \
-                                        <input type="text" name="pollChoice" > \
+                                        <input type="text" id="choice1" name="pollChoice" > \
                                     </li> \
                                     <li> \
-                                        <input type="text" name="pollChoice" > \
+                                        <input type="text" id="choice2" name="pollChoice" > \
                                         <input type="button" id="add_question" onclick="addChoiceF(event)" value="+" > \
                                     </li> \
                                 </ul> \
                 </li>');
+
+    ref_num++;
+    var question_name = 'pollQuestion_'.concat(ref_num.toString());
+    question_block.find("#question").attr('name',question_name);
+
+    ref_num++;
+    var choice_1_name = 'pollChoice_'.concat(ref_num.toString());
+    question_block.find("#choice1").attr('name',choice_1_name);
+
+    ref_num++;
+    var choice_2_name = 'pollChoice_'.concat(ref_num.toString());
+    question_block.find("#choice2").attr('name',choice_2_name);
+
     $("#poll_list").append(question_block);
 }
 
@@ -45,7 +58,13 @@ function removeQuestionF(event)
 
 function addChoiceF(event)
 {
-    $(event.target).parent().after(choice_block.clone());
+    var choice_num = choice_block.clone();
+
+    ref_num++;
+    var choice_name = 'pollChoice_'.concat(ref_num.toString());
+    choice_num.find("#choice").attr('name',choice_name);
+
+    $(event.target).parent().after(choice_num);
 }
 
 //============================================================
