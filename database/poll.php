892f75f82b45f4260930095df03c9762b902801b
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-11-28 19:35:37 +0000

    Added insertion of multiple questions and choices in the database

--- database/poll.php
+++ database/poll.php
@@ -1,24 +1,84 @@
 <?php
 
 require_once 'sqlite.php';
+require_once 'database/validation.php';
 
 class Poll
 {
 
-    public function createPoll($idUser, $pollTitle, $sharing)
+    public function createPoll($idUser, $formInfo, $img_tmp_name)
     {
+        $validTitle = cleanInput($formInfo['pollTitle']);
+
         if(!isset($sharing))
         {
             $sharing = 'public';
         }
 
+        //Create poll in database
         $sqlite = new SQLite();
-        return $sqlite->insertPoll($idUser, $pollTitle, $sharing);
+        $idPoll = $sqlite->insertPoll($idUser, $validTitle, $sharing);
+
+        //Save poll image
+        $this->saveImgPoll($idPoll, $img_tmp_name);
+
+        //Save questions and choices
+        $this->createQuestions($idPoll, $formInfo);
     }
+
+    //========================================
 
     public function saveImgPoll($idPoll, $tmp_name)
     {
         $originalFileName = "images/originals/$idPoll.jpg";
         move_uploaded_file($tmp_name, $originalFileName);
     }
+
+    //========================================
+
+    public function createQuestions($idPoll, $formInfo)
+    {
+        $formKeys = array_keys($formInfo);
+        $arrayPos = 0;
+        $choicesArray = null;
+
+        foreach($formKeys as $key)
+        {
+            $charPos = strpos($key, '_');
+            $keyType = substr($key, 0, $charPos);
+
+            if($keyType === 'pollQuestion')
+            {
+                if(!isset($questionText))
+                {
+                    $questionText = cleanInput($formInfo[$key]);
+                }
+                else
+                {
+                    $sqlite = new SQLite();
+                    $sqlite->insertQuestion($idPoll, $questionText, $choicesArray);
+                    $questionText = cleanInput($formInfo[$key]);
+                    unset($choicesArray);
+                    $arrayPos = 0;
+                }
+            }
+
+            if($keyType === 'pollChoice')
+            {
+                if(!empty($formInfo[$key]))
+                {
+                    $choicesArray[$arrayPos] = cleanInput($formInfo[$key]);
+                    $arrayPos = $arrayPos + 1;
+                }
+            }
+        }
+
+        if(isset($questionText) && isset($choicesArray))
+        {
+            $sqlite = new SQLite();
+            $sqlite->insertQuestion($idPoll, $questionText, $choicesArray);
+        }
+    }
+
+
 }
--- save_poll.php
+++ save_poll.php
@@ -3,16 +3,12 @@
 session_set_cookie_params(0);
 session_start();
 
-require_once 'database/validation.php';
 require_once 'database/poll.php';
 
-$valid = new Validation();
-$validTitle = $valid->cleanInput($_POST['pollTitle']);
-
 $poll = new Poll();
-$idPoll = $poll->createPoll($_SESSION['userId'], $validTitle, $_POST['privatePoll']);
-$poll->saveImgPoll($idPoll, $_FILES['picture']['tmp_name']);
+$poll->createPoll($_SESSION['userId'], $_POST, $_FILES['picture']['tmp_name']);
 
+header('Location: showPolls.php');
 
 ?>
 
