dfedc0fc7595697c58060b2f7acbf9fff3eb431c
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-12-01 20:57:24 +0000

    CSS updated in answer poll

--- answerPoll.php
+++ answerPoll.php
@@ -23,48 +23,49 @@ include ('templates/header.php');
 
 ?>
 
+    <section id="answer_form">
+        <form action="save_vote.php" method="POST">
+            <h1><?= $poll['title']?></h1>
+            <img src="<?= 'images/originals/'.$poll['idPoll'].'.jpg'?>" alt="poll image">
 
-<form action="save_vote.php" method="POST">
-    <h1><?= $poll['title']?></h1>
-    <img src="<?= 'images/originals/'.$poll['idPoll'].'.jpg'?>" alt="poll image">
+            <ul>
+                <?php
+                foreach($pollQuestions as $questionArray)
+                {
+                    $firstElem = true;
+                    $questionId = ''; ?>
 
-    <ul>
-    <?php
-    foreach($pollQuestions as $questionArray)
-    {
-        $firstElem = true;
-        $questionId = ''; ?>
-
-
-            <?php
-            foreach($questionArray as $questionElem)
-            {
-                if($firstElem)
-                {?>
-                    <li><h2><?= $questionElem['question']?></h2></li>
-                    <ul>
 
                     <?php
-                    $questionId = $questionElem['idPollQuestion'];
-                    $firstElem = false;
-                }
-                else
-                {?>
-                    <li>
-                        <input type="radio" name="questionId_<?= $questionId?>" value="<?= $questionElem['idPollChoice']?>" >
-                        <?php echo $questionElem['choice']; ?>
-                    </li>
+                    foreach($questionArray as $questionElem)
+                    {
+                        if($firstElem)
+                        {?>
+                            <li><h2><?= $questionElem['question']?></h2></li>
+                            <ul>
+
+                            <?php
+                            $questionId = $questionElem['idPollQuestion'];
+                            $firstElem = false;
+                        }
+                        else
+                        {?>
+                            <li>
+                                <input type="radio" name="questionId_<?= $questionId?>" value="<?= $questionElem['idPollChoice']?>" >
+                                <?php echo $questionElem['choice']; ?>
+                            </li>
+                        <?php
+                        }
+                    }
+                    ?>
+                    </ul>
                     <?php
+                    $firstElem = true;
                 }
-            }
-            ?>
-        </ul>
-<?php
-        $firstElem = true;
-    }
-    ?>
-    </ul>
-    <input type="submit" value="Submit">
-</form>
+                ?>
+            </ul>
+            <input type="submit" value="Submit">
+        </form>
+    </section>
 
 <?php include ('templates/footer.php'); ?>
