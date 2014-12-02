2715ab6939e7e8f8d8f505de28739cfaf73c4d7c
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-12-02 10:35:24 +0000

    Register vote on pollVotes array of the SESSION

--- save_vote.php
+++ save_vote.php
@@ -8,6 +8,11 @@ require_once 'database/poll.php';
 $poll = new Poll();
 $poll->submitPoll($_POST);
 
-header('Location: view_user.php');
+// Add poll ID to voted poll array
+$_SESSION['pollVotes'][] = $_POST['idPoll'];
+
+//Redirect to poll results
+$linkResult = "results_poll?id=".$_POST['idPoll'];
+header("Location: $linkResult");
 
 ?>
