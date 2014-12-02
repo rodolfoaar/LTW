2376d05ec87a2b6216aca67ae53cb20a780cda67
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-12-02 12:00:51 +0000

    Added verification to protect access to save poll page

--- save_poll.php
+++ save_poll.php
@@ -5,6 +5,12 @@ session_start();
 
 require_once 'database/poll.php';
 
+if(!isset($_POST))
+{
+    header('Location: index.php');
+    die();
+}
+
 $poll = new Poll();
 $poll->createPoll($_SESSION['userId'], $_POST, $_FILES['picture']['tmp_name']);
 
