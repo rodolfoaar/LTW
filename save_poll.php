85ae4447ecded807a7a28ebda4c63447ff1bf03a
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-12-02 10:34:46 +0000

    Changed redirect on save_poll

--- save_poll.php
+++ save_poll.php
@@ -8,7 +8,7 @@ require_once 'database/poll.php';
 $poll = new Poll();
 $poll->createPoll($_SESSION['userId'], $_POST, $_FILES['picture']['tmp_name']);
 
-header('Location: showPolls.php');
+header('Location: view_user.php');
 
 ?>
 
